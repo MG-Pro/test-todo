dbb954d90863933b12609a764836ce87822ef769
MG <mg84@bk.ru>
2019-02-05 20:34:24 +0300

    Updates

---
+++ precache-manifest.6524b1efa099d57a71e97c368f65dfd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaf420eb51cb03e40239",
+    "url": "/ra-1-MG-Pro/static/js/runtime~main.eaf420eb.js"
+  },
+  {
+    "revision": "a96b838d227e10fd9549",
+    "url": "/ra-1-MG-Pro/static/js/main.a96b838d.chunk.js"
+  },
+  {
+    "revision": "861f2d39e8fb1eca28d1",
+    "url": "/ra-1-MG-Pro/static/js/1.861f2d39.chunk.js"
+  },
+  {
+    "revision": "a96b838d227e10fd9549",
+    "url": "/ra-1-MG-Pro/static/css/main.69db77a3.chunk.css"
+  },
+  {
+    "revision": "861f2d39e8fb1eca28d1",
+    "url": "/ra-1-MG-Pro/static/css/1.cd7e89e3.chunk.css"
+  },
+  {
+    "revision": "29de312b0a316ea89844a784f30c0811",
+    "url": "/ra-1-MG-Pro/index.html"
+  }
+];
